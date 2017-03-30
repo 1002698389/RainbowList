85ef8f7c1e0442173d24b6d369bb159e574cb82c
admin <admin@MacBook-Pro.local>
2017-03-31 00:49:56 +0800

    about app page

---
+++ RainbowList/Controllers/AboultViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AboultViewController.swift
+//  RainbowList
+//
+//  Created by admin on 2017/3/30.
+//  Copyright © 2017年 aLazyCoder. All rights reserved.
+//
+
+import UIKit
+
+class AboultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
